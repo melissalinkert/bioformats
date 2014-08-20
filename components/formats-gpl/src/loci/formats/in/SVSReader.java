3676a3d7001ca6f0172f30bd373108212289d0e8
Melissa Linkert <melissa@glencoesoftware.com>
2014-08-20 14:05:28 -0500

    Adjust exposure time to account for exposure scale

--- components/formats-gpl/src/loci/formats/in/SVSReader.java
+++ components/formats-gpl/src/loci/formats/in/SVSReader.java
@@ -439,7 +439,7 @@ public class SVSReader extends BaseTiffReader {
   }
 
   protected Double getExposureTime() {
-    return exposureTime;
+    return exposureTime * exposureScale * 1000;
   }
 
   protected Timestamp getDatestamp() {

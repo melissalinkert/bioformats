cb592b21c5b11fb8cdd32949766b5292c92aa837
Melissa Linkert <melissa@glencoesoftware.com>
2021-04-28 18:12:02 -0500

    Fix units deprecation warning

--- components/formats-gpl/src/loci/formats/in/LeicaSCNReader.java
+++ components/formats-gpl/src/loci/formats/in/LeicaSCNReader.java
@@ -391,8 +391,8 @@ public class LeicaSCNReader extends BaseTiffReader {
         // Leica units are nanometres; convert to µm
         double sizeX = i.vSizeX / 1000.0;
         double sizeY = i.vSizeY / 1000.0;
-        final Length offsetX = new Length(i.vOffsetX, UNITS.NM);
-        final Length offsetY = new Length(i.vOffsetY, UNITS.NM);
+        final Length offsetX = new Length(i.vOffsetX, UNITS.NANOMETRE);
+        final Length offsetY = new Length(i.vOffsetY, UNITS.NANOMETRE);
         double sizeZ = i.vSpacingZ / 1000.0;
 
         store.setPixelsPhysicalSizeX(
